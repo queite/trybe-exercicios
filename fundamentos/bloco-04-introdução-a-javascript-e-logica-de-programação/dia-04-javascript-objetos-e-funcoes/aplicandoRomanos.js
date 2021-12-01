392aacbebcb371b1a65ec9a2dc3873748b72d7bc
Queite <queitesc@gmail.com>
2021-12-01 18:42:16 -0300

    Adiciona exercicio romanos para entendimento

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programação/dia-04-javascript-objetos-e-funcoes/aplicandoRomanos.js
@@ -0,0 +1,13 @@
+function romanosParaDecimais(romano) {
+  let conversao = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  romano = romano.toUpperCase();
+  let 
+}
