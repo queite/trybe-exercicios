9be5e3e577a9c0990459d5206ca63ab48f979436
Queite <queitesc@gmail.com>
2022-01-03 16:00:50 -0300

    Ends exercices

--- fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-function/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-function/exercicios.js
@@ -1,3 +1,4 @@
+// Exercício 1
 const EmployeesInformation = (name) => {
   return {name, email: `${name.toLowerCase().split(' ').join('_')}@trybe.com`}
 }
@@ -12,3 +13,55 @@ const newEmployees = () => {
 };
 
 console.log(newEmployees());
+
+// Exercício 2
+const validation = (number, random) => number === random;
+
+const sort = (number, func) => {
+  const random = Math.floor(Math.random() * 5 + 1);
+  return func(number, random) ? 'Parabéns você ganhou':'Tente novamente'
+}
+console.log(sort(2, validation));
+
+// Exercício 3
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+// const check = (gabarito, respostas) => {
+//   let count = 0;
+//   for (let i = 0; i < gabarito.length; i++) {
+//     if (gabarito[i] === respostas[i]) {
+//       count += 1;
+//     }else if (respostas[i] === 'N.A') {
+//       count += 0;
+//     } else {
+//       count -= 0.5;
+//     }
+//   }
+//   return count
+// }
+
+// const result = (gabarito, respostas, func) => func(gabarito, respostas);
+// console.log(result(RIGHT_ANSWERS, STUDENT_ANSWERS, check));
+
+const addPoint = (gabarito, respostas) => {
+  if (gabarito === respostas) {
+    return 1;
+  }
+  if (respostas === 'N.A') {
+    return 0;
+  }
+  return -0.5
+}
+
+const result = (gabarito, respostas, func) => {
+  let count = 0;
+  for (let i = 0; i < gabarito.length; i++) {
+    const addPointReturn = func(gabarito[i], respostas[i]);
+    count += addPointReturn;
+  }
+  return `${count} pontos`
+}
+
+console.log(result(RIGHT_ANSWERS, STUDENT_ANSWERS, addPoint))
+
