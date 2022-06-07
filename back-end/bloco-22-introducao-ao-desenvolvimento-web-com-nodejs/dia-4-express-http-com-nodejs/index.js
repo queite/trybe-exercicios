3f762122fb72b15826138012583dc07128573934
Queite Schneider Castiglioni <qsc@LAPTOP-BEM3OU72.localdomain>
2022-06-06 21:33:34 -0300

    :sparkles: task 4

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -8,9 +8,16 @@ app.post('/greetings', function (req, res) {
   const {name, age} = req.body;
   if (age > 17) return res.status(200).json({ "message": `Hello, ${name}!`});
   return res.status(401).json({ "message": "Unauthorized" });
+  //httpie =>  http :3000/greetings name=Queite age:=25
 });
 
+app.put('/users/:name/:age', function (req, res) {
+  const { name, age } = req.params;
 
+  return res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` });
+  // httpie => http PUT :3000/users/Queite/35  
+});
+     
 
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3000');
