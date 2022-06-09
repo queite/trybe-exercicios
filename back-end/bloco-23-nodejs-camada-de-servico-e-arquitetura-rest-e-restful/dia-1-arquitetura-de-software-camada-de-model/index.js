23084fc29c7bc1fcd3b4d4e4b41a2d34c0a5a594
Queite Schneider Castiglioni <qsc@LAPTOP-BEM3OU72.localdomain>
2022-06-09 19:09:04 -0300

    :sparkles: feat: done tasks 1-4

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
@@ -7,6 +7,28 @@ const User = require('./models/User');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/user', async (_req, res) => {
+  const users = await User.getAllUsers();
+  return res.status(200).json(users);
+});
+
+app.get('/user/:id', async (req, res) => {
+  const {id} = req.params;
+  console.log(id);
+  const user = await User.getUserById(id);
+  if (!user) return res.status(404).json({"error": true, "message": "Usuário não encontrado"});
+  return res.status(200).json(user);
+});
+
+app.put('/user/:id', validateUser(schemas.users), async (req, res) => {
+  const {id} = req.params;
+  const {firstName, lastName, email, password} = req.body;
+  const user = await User.updateUser(id, firstName, lastName, email, password);
+  console.log(user);
+  if (!user.length) return res.status(404).json({"error": true, "message": "Usuário não encontrado"});
+  return res.status(200).json(user);
+});
+
 app.post('/user', validateUser(schemas.users), async (req, res) => {
   const {firstName, lastName, email, password} = req.body;
   const newUSer = await User.createUser(firstName, lastName, email, password);
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/User.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/models/User.js
@@ -1,9 +1,50 @@
+const { use } = require('express/lib/application');
 const connection = require('./connection');
 
+// Cadastra usuário
 const createUser = (firstName, lastName, email, password) => connection.execute('INSERT INTO users_crud.users (first_name, last_name, email, password) VALUES (?,?,?,?)',
   [firstName, lastName, email, password])
-    .then(([result]) => ({ id: result.insertId, firstName, lastName, email }));
+    .then(([result]) => ({ id: result.insertId, firstName, lastName, email })); // essa última linha retorna o usuário cadastrado. https://thewebdev.info/2022/03/06/how-to-retrieve-the-last-inserted-id-with-node-js-mysql/
+
+// Converte o nome dos campos de snake_case para camelCase
+const serialize = (data) => ({
+  id: data.id,
+  firstName: data.first_name,
+  lastName: data.last_name,
+  email: data.email,
+  password: data.password
+});
+
+// Busca todos usuários do banco
+const getAllUsers = async () => {
+  const [users] = await connection.execute(
+    'SELECT * FROM users_crud.users;',
+  );
+  return users.map(serialize);
+};
+
+// Busca usuário por ID
+const getUserById = async (id) => {
+  const [users] = await connection.execute(
+    'SELECT * FROM users_crud.users WHERE id=?;', [Number(id)]
+  );
+  console.log(users);
+  return users.map(serialize);
+};
+
+// Atualiza usuário
+const updateUser = async (id, firstName, lastName, email, password) => {
+  await connection.execute(
+    'UPDATE users_crud.users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ?',
+    [firstName, lastName, email, password, id]
+  );
+
+  return await getUserById(id);
+};
 
 module.exports = {
   createUser,
+  getAllUsers,
+  getUserById,
+  updateUser
 }
