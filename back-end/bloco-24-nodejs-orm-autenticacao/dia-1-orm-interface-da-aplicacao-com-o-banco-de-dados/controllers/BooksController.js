7534f89b4748fe62eb86b055bfcfae05d297680f
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-06-28 09:13:52 -0300

    Adiciona rota get /books/:id e post /books

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/controllers/BooksController.js
@@ -3,10 +3,25 @@ const BookService = require('../services/BooksService');
 
 const getAll = async (_req, res) => {
   const books = await BookService.getAll();
-
   res.status(200).json(books);
+};
+
+const getById = async (req, res) => {
+  const {id} = req.params;
+  const book = await BookService.getById(id);
+
+  if (!book) return res.status(404).json({ "message": "Book not found" });
+  res.status(200).json(book);
+};
+
+const create = async (req,res) => {
+  const { title, author, pageQuantity } = req.body;
+  const newBook = await BookService.create({ title, author, pageQuantity });
+  res.status(201).json(newBook);
 }
 
 module.exports = {
   getAll,
+  getById,
+  create,
 }
