0edfd9a172668953d4af2324f998c6164db5feff
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-07-19 17:40:47 -0300

    exercícios

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/length.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/length.ts
@@ -10,12 +10,12 @@ function convert (value: number, baseUnit: string, convertionUnit: string){
 }
 
 export default function exec () {
-  const value = readline.question('Digite o valor a ser convertido: ');
+  const value = readline.questionFloat('Digite o valor a ser convertido: ');
   const baseUnitIndex = readline.keyInSelect(units, "Escolha a unidade atual ");
   const convertionUnitIndex = readline.keyInSelect(units, "Escolha a unidade de destino ");
   const baseUnit = units[baseUnitIndex];
   const convertionUnit = units[convertionUnitIndex];
-  console.log(`${value}${baseUnit} é igual a ${convert(Number(value), baseUnit, convertionUnit)}${convertionUnit}.`)
+  console.log(`${value}${baseUnit} é igual a ${convert(value, baseUnit, convertionUnit)}${convertionUnit}.`)
 }
 
 exec()
