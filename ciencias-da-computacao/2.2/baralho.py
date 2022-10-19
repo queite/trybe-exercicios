cc64646c4d99e56fa8d5b755344f0046b28a7a37
Queite <queitesc@gmail.com>
2022-10-19 15:58:22 -0300

    docs: :memo: entendendo iterator e iterable exercício baralho

--- ciencias-da-computacao/2.2/baralho.py
+++ ciencias-da-computacao/2.2/baralho.py
@@ -8,10 +8,36 @@ class Carta:
 
     def __repr__(self):
         return '<%s de %s>' % (self.valor, self.naipe)
+        # https://pyformat.info/#simple
+'''
+O __str__ serve para exibir o objeto para usuário final, usada pelo comando print e pela função str
+O __repr__ serve para exibir o objeto para o programador, usada pelo console do Python e pela funçao repr.
+Ex.:
+import datetime
+today = datetime.datetime.now()
+str(today)
+'2012-03-14 09:21:58.130922'
+repr(today)
+'datetime.datetime(2012, 3, 14, 9, 21, 58, 130922)'
+''' # noqa
 
+# __iter__ = estado que armazena a posição
+class IteradorDoBaralho(Iterator):  # Iterator introduz o dunder/magic method __iter__ na classe # noqa
+    def __init__(self, cartas):
+        self._cartas = cartas
+        self._index = 0  # estado que armazena a posição
 
-class IteradorDoBaralho(Iterator):
-    ...
+    # essa linha faz o mesmo que o Iterator:
+    # def __iter__(self):
+    #     return self
+
+    def __next__(self):  # é isso que efetivamente cria o iterador
+        try:
+            carta = self._cartas[self._index]
+        except IndexError:
+            raise StopIteration
+        self._index += 1
+        return carta
 
 
 class Baralho(Iterable):
@@ -28,7 +54,18 @@ class Baralho(Iterable):
     def __len__(self):
         return len(self._cartas)
 
+    def __iter__(self):
+        return IteradorDoBaralho(self._cartas)
+
+    def __str__(self):
+        return f'{self._cartas}'
+
+
+# baralho = Baralho()
+# for carta in baralho:
+#     print(carta)
+
 
 baralho = Baralho()
-for carta in baralho:
-    print(carta)
+print(baralho)
+# print(dir(Baralho))
